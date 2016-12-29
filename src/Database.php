24b89521ece357c67f53da46a1535fe9d530c0ad
cihaneryonucu <eryonucu@gmail.com>
2016-12-29 23:16:10 +0300

    Login

--- src/Database.php
+++ src/Database.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Cihan
@@ -8,12 +9,14 @@
 class Database
 {
     var $conn;
+
     function __construct()
     {
-        $servername = "139.179.206.167:3306";//this is for my machine we can change it?
-        $username = "Tcan";
+        $servername = "localhost";//this is for my machine we can change it?
+        $username = "root";
         $password = "123123";
         $dbname = "CS353";
+
         // Create connection
         $this->conn = new mysqli($servername, $username, $password, $dbname);
         // Check connection
@@ -21,39 +24,59 @@ class Database
             die("Connection failed: " . $this->conn->connect_error);
         }
         else
-            echo "Connected to database";
+            echo "Connected to database\n";
     }
+
     //Name,birthdate,desc,email,country,username our all strings, password is int.
     //Also do we need an username? It can be just email too. todo
     function register($name, $birtdate, $desc, $email, $nickname, $country,$username,$password)
     {
         $sql = "INSERT INTO USER(name, birth_date, description, email, nickname,country) 
                 VALUES ('$name', '$birtdate', '$desc', '$email', '$nickname', '$country');";
+
         if ($this->conn->query($sql) === TRUE) {
             echo "New record created successfully\n";
         } else {
             echo "Error: " . $sql . "<br>" . $this->conn->error;
         }
+
+
+
         $sql = "SELECT count(iduser) as count FROM USER GROUP BY iduser";
         $result = $this->conn->query($sql);
+
         $row = $result->num_rows;
         $sql = "INSERT INTO Login_system(username, password, idofUser) 
                 VALUES ('$username', $password, $row )";
+
         if ($this->conn->query($sql) === TRUE) {
             echo "New record created successfully\n";
         } else {
             echo "Error: " . $sql . "<br>" . $this->conn->error;
         }
     }
-    function login($name, $birtdate, $desc, $email, $nickname, $country,$username,$password)
+
+    function login($username,$password)
     {
-        $sql = "INSERT INTO USER(name, birth_date, description, email, nickname,country) 
-                VALUES ($name, $birtdate,$desc, $email, $nickname,$country)";
-        if ($this->conn->query($sql) === TRUE) {
-            echo "New record created successfully\n";
+
+        $sql = "SELECT *
+                FROM login_system
+                WHERE (username = '$username') AND (password = $password) ";
+
+        $result = $this->conn->query($sql);
+        echo "result: " . $result->num_rows;
+
+        if ($result->num_rows > 0) {
+            echo "Logged in successfully\n";
+            return 1;
         } else {
-            echo "Error: " . $sql . "<br>" . $this->conn->error;
+            echo "No user";
+            return -1;
         }
     }
+
+
+
 }
+
 ?>
